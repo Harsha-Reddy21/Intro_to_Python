689b085a6ce49829bc328f31cc54201cb46f452e
Harsha Reddy <harshareddygaddam21@gmail.com>
2025-07-22 15:31:52 +0530

    Problem1

---
+++ personal_information_collector.py
@@ -0,0 +1,13 @@
+name=input("Enter your full name: ")
+age=input("Enter your age: ")
+city=input("Enter your city: ")
+hobby=input("Enter your hobby: ")
+
+print(f"Hello {name}, \nYou are {age} years old and you live in {city}. \nYour hobby is {hobby}.")
+
+
+
+
+
+
+
