5d477a954cded8eb041146bf5450c9a04554daf4
Harsha Reddy <harshareddygaddam21@gmail.com>
2025-07-22 17:10:03 +0530

    Multi format string displayer

---
+++ multi_format_string_displayer.py
@@ -0,0 +1,17 @@
+name='Harsha'
+age=22
+city='Hyderabad'
+
+print("% formatting")
+print("Name: %s, Age: %s, City: %s" % (name, age, city))
+print()
+
+
+print(".format")
+print("Name: {}, Age: {}, City: {}".format(name, age, city))
+print()
+
+
+print("f-string")
+print(f"Name: {name}, Age: {age}, City: {city}")
+print()
